0b26ddaf10b8651af128ab2122754479ad8aa604
Vladimír Ulman <ulman@fi.muni.cz>
2026-06-24 15:07:56 +0200

    ADD1/2: exampleCircles2D features DemoLutConnector from which...
    ...it borrows/takes its underlying Converter, and creates SAC
    with that converter; the plan is that LUT editor would change
    behaviour of the underlying Converter and that way changes the
    display/view of the SAC on the screen

--- src/test/java/bdv/util/ExampleCircles2D.java
+++ src/test/java/bdv/util/ExampleCircles2D.java
@@ -1,10 +1,24 @@
 package bdv.util;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
+
+import bdv.tools.brightness.ConverterSetup;
+import bdv.tools.brightness.lut.DemoLutConnector;
+import bdv.tools.brightness.lut.DemoLutConnectorConverterSetup;
+import bdv.viewer.SourceAndConverter;
+import mpicbg.spim.data.sequence.FinalVoxelDimensions;
 import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converter;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.realtransform.AffineTransform3D;
+import net.imglib2.type.numeric.ARGBType;
+import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.view.Views;
 
 /**
  * Generates random circles in 2D
@@ -52,6 +66,33 @@ public class ExampleCircles2D {
             }
         }
 
-        BdvFunctions.show(img, "Random Gray Circles Test", Bdv.options().is2D());
+        // create a LUTeditor 'lutEditor' that can do this:
+        // final Converter<UnsignedByteType, ARGBType> converter = lutEditor.getConverter();
+        // (so, this an editor that, based on its current settings, modifies its underlying Converter,
+        // and we borrow its Converter to the BDV)
+        DemoLutConnector<UnsignedByteType> demoLUT = new DemoLutConnector<>(img);
+        final Converter<UnsignedByteType, ARGBType> converter = demoLUT.getConverter();
+
+        // create SAC around this converter
+        // (so that the SAC display will always go through our (by us controlled) converter)
+        final SourceAndConverter<UnsignedByteType> sac = createSAC(img, converter, "Random Gray Circles Test");
+    }
+
+    static <T extends RealType<T>>
+    SourceAndConverter<T> createSAC(final RandomAccessibleInterval<T> img,
+                                    final Converter<T,ARGBType> converter,
+                                    final String title) {
+        final double[] pixelResolutionValues = new double[img.numDimensions()];
+        Arrays.fill(pixelResolutionValues, 1.0);
+
+        final RandomAccessibleIntervalMipmapSource<T> mipmap = new RandomAccessibleIntervalMipmapSource<>(
+            new RandomAccessibleInterval[]{ Views.addDimension(img,0,0) },
+            img.getType(),
+            new AffineTransform3D[]{new AffineTransform3D()},
+            new FinalVoxelDimensions("px", pixelResolutionValues),
+            title,
+            true );
+
+        return new SourceAndConverter<T>(mipmap, converter);
     }
 }
